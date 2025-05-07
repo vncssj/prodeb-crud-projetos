ed61f0d2d037846ebd8cc9f1de93c5de2d0a089a
Vinícius Jesus <vncssj@gmail.com>
2025-05-07 15:26:29 -0300

    feat(config): set default configs to database, and removing env file from .gitignore (test case)

--- config/app_local.php
+++ config/app_local.php
@@ -36,7 +36,7 @@ return [
      */
     'Datasources' => [
         'default' => [
-            'host' => 'localhost',
+            'host' => 'db',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
@@ -44,10 +44,10 @@ return [
              */
             //'port' => 'non_standard_port_number',
 
-            'username' => 'my_app',
-            'password' => 'secret',
+            'username' => 'usr_projetos',
+            'password' => 'usr_projetos',
 
-            'database' => 'my_app',
+            'database' => 'projetos',
             /*
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
